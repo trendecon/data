71bd451ee8433bb1ef9e1254a617e3c04d17da9a
pat-s <patrick.schratz@gmail.com>
2020-05-30 11:40:22 +0200

    add tic files

---
+++ tic.R
@@ -0,0 +1,10 @@
+get_stage("install") %>%
+  add_step(step_install_github("trendecon/trendecon"))
+
+get_stage("before_deploy") %>%
+  add_step(step_setup_ssh()) %>%
+  add_step(step_setup_push_deploy())
+
+get_stage("deploy") %>%
+  add_code_step(trendecon::proc_trendecon_ch()) %>% I
+  #add_step(step_do_push_deploy())
