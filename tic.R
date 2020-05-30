2e0aa64e9de73008024c2333117a0bb909eadca6
pat-s <patrick.schratz@gmail.com>
2020-05-30 12:50:37 +0200

    install in deploy stage

--- tic.R
+++ tic.R
@@ -1,10 +1,8 @@
-get_stage("install") %>%
-  add_step(step_install_github("trendecon/trendecon"))
-
 get_stage("before_deploy") %>%
   add_step(step_setup_ssh()) %>%
   add_step(step_setup_push_deploy())
 
 get_stage("deploy") %>%
+  add_step(step_install_github("trendecon/trendecon")) %>%
   add_code_step(withr::with_package("trendecon", proc_trendecon_ch())) %>% I
   #add_step(step_do_push_deploy())
